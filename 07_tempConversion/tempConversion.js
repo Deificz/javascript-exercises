8586640b225dcb996488c12eff46029334dbb318
Deificz <99456747+Deificz@users.noreply.github.com>
2023-07-03 17:00:04 +0800

    Updated and solved the remaining tasks for Fundamentals Part 4

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let calculation =5/9 * (temp - 32);
+  let finalCalc = Number(calculation.toFixed(1));
+  return finalCalc;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let calculation =(9/5 * (temp))+32;
+  let finalCalc = Number(calculation.toFixed(1));
+  return finalCalc;
 };
 
 // Do not edit below this line
